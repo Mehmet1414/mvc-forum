8b9069bce962fded543557e9646912068c365569
Mehmet Aydin <110032695+Mehmet1414@users.noreply.github.com>
2023-08-02 00:29:28 +0200

    login sarti güncellendddi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import AddPostControler from "./pages/addPost/AddPostControler";
@@ -8,22 +8,26 @@ import LoginControler from "./pages/login/LoginControler";
 import RegisterControler from "./pages/register/RegisterControler";
 
 function App() {
-const [userData,setUserData]=useState(null)
-  console.log(userData)
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(localStorage.getItem("user"));
+  console.log(userData);
+  useEffect(() => {
+    if (!userData) {
+      navigate("/");
+    } else {
+      navigate("/list_post");
+    }
+  }, [userData]);
   return (
     <>
       <Header setUserData={setUserData} />
       <Routes>
-        {!userData ? (
-          <Route path="/" element={<LoginControler setUserData={setUserData} />} />
-
-        ):(
-          <>
-          
-          <Route path="/list_post" element={<ListPostControler />} />
-          <Route path="/add_post" element={<AddPostControler />} />
-          </>
-        )}
+        <Route
+          path="/"
+          element={<LoginControler setUserData={setUserData} />}
+        />
+        <Route path="/list_post" element={<ListPostControler />} />
+        <Route path="/add_post" element={<AddPostControler />} />
         <Route path="/register" element={<RegisterControler />} />
       </Routes>
     </>
